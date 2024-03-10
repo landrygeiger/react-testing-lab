a3392a1e98b11b8785e786566bdb40966b6c1fad
ZWally <zach.wallenburg@icloud.com>
2024-03-09 21:45:35 -0600

    added test for like becomes unlike

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,15 +1,25 @@
 import {render, screen} from '@testing-library/react'
 import App from '../src/App'
+import userEvent from '@testing-library/user-event'
 import "@testing-library/jest-dom"
 
 
-describe("my test suite", () => {
-  it("not yet implemented", async ()=>{
+describe("In my web forum", () => {
+  it("the like button should become unlike after clicking", async ()=>{
     render(<App />)
 
-    await screen.findByRole('heading')
 
-    expect(screen.getByRole('heading')).toBeInTheDocument()
+    //Make sure on load the page renders with the "Like" button
+    expect(screen.queryByText('Unlike')).toBeFalsy()
+    expect(screen.getByText('Like')).toBeInTheDocument()
+
+    //click the button
+    await userEvent.click(screen.getByText('Like'))
+
+    //It is replaced with the "Unlike" button
+    expect(screen.queryByText('Like')).toBeFalsy()
+    expect(screen.getByText('Unlike')).toBeInTheDocument()
+
   })
 
 });
