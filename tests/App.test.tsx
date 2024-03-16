938e50aabcd5b86c635bfa3d99bb94663e9f8c07
ZWally <zach.wallenburg@icloud.com>
2024-03-16 17:43:12 -0500

    added tests for increment/decrement counter

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -22,4 +22,47 @@ describe("In my web forum", () => {
 
   })
 
+  it("the like button should increment number of likes after clicking", async ()=>{
+    render(<App />)
+
+    
+    const likesRegex = /^\d+ Likes$/;
+
+    const originalNumberOfLikes:number = parseInt(screen.getByText(likesRegex).textContent!.split(" ")[0]);
+    
+
+
+    //click the button
+    await userEvent.click(screen.getByText('Like'))
+
+    const updatedNumberOfLikes:number = parseInt(screen.getByText(likesRegex).textContent!.split(" ")[0]);
+
+    
+    expect(updatedNumberOfLikes).toEqual(originalNumberOfLikes + 1);
+    
+  })
+
+  it("the unlike button should decrement number of likes after clicking", async ()=>{
+    render(<App />)
+
+    
+    const likesRegex = /^\d+ Likes$/;
+    
+    //click the button to add a like
+    await userEvent.click(screen.getByText('Like'))
+    expect(screen.queryByText('Like')).toBeFalsy()
+    expect(screen.getByText('Unlike')).toBeInTheDocument()
+
+    const originalNumberOfLikes:number = parseInt(screen.getByText(likesRegex).textContent!.split(" ")[0]);
+
+    //click the button again to remove the like
+    await userEvent.click(screen.getByText('Unlike'))
+
+    const updatedNumberOfLikes:number = parseInt(screen.getByText(likesRegex).textContent!.split(" ")[0]);
+
+    
+    expect(updatedNumberOfLikes).toEqual(originalNumberOfLikes - 1);
+    
+  })
+
 });
