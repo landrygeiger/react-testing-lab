7fc512f760886ac05478cfa702951378e30e2065
ZWally <zach.wallenburg@icloud.com>
2024-03-17 18:21:10 -0500

    added semicolons

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -6,24 +6,24 @@ import "@testing-library/jest-dom"
 
 describe("In my web forum", () => {
   it("the like button should become unlike after clicking", async ()=>{
-    render(<App />)
+    render(<App />);
 
 
     //Make sure on load the page renders with the "Like" button
-    expect(screen.queryByText('Unlike')).toBeFalsy()
-    expect(screen.getByText('Like')).toBeInTheDocument()
+    expect(screen.queryByText('Unlike')).toBeFalsy();
+    expect(screen.getByText('Like')).toBeInTheDocument();
 
     //click the button
-    await userEvent.click(screen.getByText('Like'))
+    await userEvent.click(screen.getByText('Like'));
 
     //It is replaced with the "Unlike" button
-    expect(screen.queryByText('Like')).toBeFalsy()
-    expect(screen.getByText('Unlike')).toBeInTheDocument()
+    expect(screen.queryByText('Like')).toBeFalsy();
+    expect(screen.getByText('Unlike')).toBeInTheDocument();
 
   })
 
   it("the like button should increment number of likes after clicking", async ()=>{
-    render(<App />)
+    render(<App />);
 
     
     const likesRegex = /^\d+ Likes$/;
@@ -33,7 +33,7 @@ describe("In my web forum", () => {
 
 
     //click the button
-    await userEvent.click(screen.getByText('Like'))
+    await userEvent.click(screen.getByText('Like'));
 
     const updatedNumberOfLikes:number = parseInt(screen.getByText(likesRegex).textContent!.split(" ")[0]);
 
@@ -43,20 +43,21 @@ describe("In my web forum", () => {
   })
 
   it("the unlike button should decrement number of likes after clicking", async ()=>{
-    render(<App />)
+    render(<App />);
 
     
     const likesRegex = /^\d+ Likes$/;
     
     //click the button to add a like
-    await userEvent.click(screen.getByText('Like'))
-    expect(screen.queryByText('Like')).toBeFalsy()
-    expect(screen.getByText('Unlike')).toBeInTheDocument()
+    await userEvent.click(screen.getByText('Like'));
+
+    expect(screen.queryByText('Like')).toBeFalsy();
+    expect(screen.getByText('Unlike')).toBeInTheDocument();
 
     const originalNumberOfLikes:number = parseInt(screen.getByText(likesRegex).textContent!.split(" ")[0]);
 
     //click the button again to remove the like
-    await userEvent.click(screen.getByText('Unlike'))
+    await userEvent.click(screen.getByText('Unlike'));
 
     const updatedNumberOfLikes:number = parseInt(screen.getByText(likesRegex).textContent!.split(" ")[0]);
 
@@ -66,83 +67,85 @@ describe("In my web forum", () => {
   })
 
   it("the Comment button should be disabled on load", async ()=>{
-    render(<App />)
+    render(<App />);
 
     expect(screen.getByText('Comment')).toBeDisabled();
     
   })
 
   it("the Comment button should be enabled once text has been input but not submitted", async ()=>{
-    render(<App />)
+    render(<App />);
 
     expect(screen.getByLabelText('Submit Comment')).toBeDisabled();
-    const input = screen.getByLabelText('Comment Input')
+    const input = screen.getByLabelText('Comment Input');
+
     await userEvent.type(input, "Test String");
+
     expect(screen.getByLabelText('Submit Comment')).toBeEnabled();
     
   })
 
   it("the Comment button should be disabled once text has been input and submitted", async ()=>{
-    render(<App />)
+    render(<App />);
 
     expect(screen.getByLabelText('Submit Comment')).toBeDisabled();
-    const input = screen.getByLabelText('Comment Input')
+    const input = screen.getByLabelText('Comment Input');
     await userEvent.type(input, "Test String");
     expect(screen.getByLabelText('Submit Comment')).toBeEnabled();
 
-    await userEvent.click(screen.getByLabelText('Submit Comment'))
+    await userEvent.click(screen.getByLabelText('Submit Comment'));
     expect(screen.getByLabelText('Submit Comment')).toBeDisabled();
     
   })
 
   it("a submitted comment should appear in the list of comments below the post", async ()=>{
-    render(<App />)
+    render(<App />);
 
     expect(screen.getByLabelText('Submit Comment')).toBeDisabled();
-    const input = screen.getByLabelText('Comment Input')
+    const input = screen.getByLabelText('Comment Input');
     await userEvent.type(input, "Test String");
     expect(screen.getByLabelText('Submit Comment')).toBeEnabled();
 
-    await userEvent.click(screen.getByLabelText('Submit Comment'))
-    expect(screen.getAllByLabelText('User Comment').includes(screen.getByText('Test String'))).toBeTruthy()
+    await userEvent.click(screen.getByLabelText('Submit Comment'));
+    expect(screen.getAllByLabelText('User Comment').includes(screen.getByText('Test String'))).toBeTruthy();
     
   })
 
   it("a forum post's text should be black on load", async ()=>{
-    render(<App />)
-    const post = screen.getByLabelText("Forum Post Content")
+    render(<App />);
+    const post = screen.getByLabelText("Forum Post Content");
 
-    const textColor = window.getComputedStyle(post).getPropertyValue("color")
-    expect(textColor).toEqual("black")
+    const textColor = window.getComputedStyle(post).getPropertyValue("color");
+    expect(textColor).toEqual("black");
     
     
   })
 
   it("a forumn post's text should be blue after clicking blue mode toggle", async ()=>{
-    render(<App />)
-    const blueModeToggle = screen.getByLabelText("Blue Mode Toggle")
-    const post = screen.getByLabelText("Forum Post Content")
-    await userEvent.click(blueModeToggle)
-    const textColor = window.getComputedStyle(post).getPropertyValue("color")
-    expect(textColor).toEqual("blue")
+    render(<App />);
+    const blueModeToggle = screen.getByLabelText("Blue Mode Toggle");
+    const post = screen.getByLabelText("Forum Post Content");
+    await userEvent.click(blueModeToggle);
+    const textColor = window.getComputedStyle(post).getPropertyValue("color");
+    expect(textColor).toEqual("blue");
     
     
   })
 
   it("a forumn post's text should be black after clicking blue mode toggle twice", async ()=>{
-    render(<App />)
-    const blueModeToggle = screen.getByLabelText("Blue Mode Toggle")
-    const post = screen.getByLabelText("Forum Post Content")
+    render(<App />);
+    const blueModeToggle = screen.getByLabelText("Blue Mode Toggle");
+    const post = screen.getByLabelText("Forum Post Content");
 
-    await userEvent.click(blueModeToggle)
+    await userEvent.click(blueModeToggle);
 
-    const textColor = window.getComputedStyle(post).getPropertyValue("color")
-    expect(textColor).toEqual("blue")
+    const textColor = window.getComputedStyle(post).getPropertyValue("color");
+    expect(textColor).toEqual("blue");
 
-    await userEvent.click(blueModeToggle)
+    await userEvent.click(blueModeToggle);
     
-    const updatedTextColor = window.getComputedStyle(post).getPropertyValue("color")
-    expect(updatedTextColor).toEqual("black")
+    const updatedTextColor = window.getComputedStyle(post).getPropertyValue("color");
+    expect(updatedTextColor).toEqual("black");
     
   })
 
