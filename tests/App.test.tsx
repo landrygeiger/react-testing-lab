d317cde2bfde43dfd67e937120901c4d06968cf5
ZWally <zach.wallenburg@icloud.com>
2024-03-17 19:11:00 -0500

    added helpful errors for unimplmented unit tests

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -44,36 +44,37 @@ describe("In my web forum", () => {
 
   it("the unlike button should decrement number of likes after clicking", async ()=>{
     //TODO: complete unit test
-    fail()
+    throw new Error('Test Not Yet Implemented')
   })
 
   it("the Comment button should be disabled on load", async ()=>{
     render(<App />);
 
-    //HELPFUL NOTE: Aria-labels are a great replacement for "test-ids" because they accomplish the same goal (A unique component identifier) while providing value to users with screen-readers.
+    //HELPFUL NOTE: Aria-labels are a great replacement for "test-ids" because they accomplish the same goal (A unique element identifier) while providing value to users with screen-readers.
     expect(screen.getByLabelText('Submit Comment')).toBeDisabled();
     
   })
 
   it("the Comment button should be enabled once text has been input but not submitted", async ()=>{
     //TODO: complete unit test (hint: use the userEvent.type method to simulate user keyboard entry! )
-    
+    throw new Error('Test Not Yet Implemented')
   })
 
   it("the Comment button should be disabled once text has been input and submitted", async ()=>{
 
     //TODO: complete unit test
-    
+    throw new Error('Test Not Yet Implemented')
   })
 
   it("a submitted comment should appear in the list of comments below the post", async ()=>{
     //TODO: complete unit test (hint: use screen.getAllByLabelText('User Comment') to get a list of HTMLElements representing the comments below a post)
-    
+    throw new Error('Test Not Yet Implemented')
   })
 
   it("a forum post's text should be black on load", async ()=>{
     render(<App />);
     const post = screen.getByLabelText("Forum Post Content");
+    
     //HELPFUL NOTE: You can use the built-in window.getComputedStyle to generate a special JSON object that details a component's css
     const textColor = window.getComputedStyle(post).getPropertyValue("color");
     expect(textColor).toEqual("black");
@@ -83,13 +84,13 @@ describe("In my web forum", () => {
 
   it("a forumn post's text should be blue after clicking blue mode toggle", async ()=>{
     //TODO: complete unit test (hint: you can use getByLabelText to isolate the blue mode checkbox)
-    
+    throw new Error('Test Not Yet Implemented')
     
   })
 
-  it("a forumn post's text should be black after clicking blue mode toggle twice", async ()=>{
+  it("a forumn post's text should be black after clicking blue mode toggle twice (toggling and then untoggling)", async ()=>{
     //TODO: complete unit test
-    
+    throw new Error('Test Not Yet Implemented')
   })
 
 });
